283d9eed7c5a504c4011800e5a7f5ffc5964e245
Louis Burke <louis.j.burke@gmail.com>
2015-12-19 12:03:08 +0000

    Close async channels

--- src/lispcast_clojure_core_async/core.clj
+++ src/lispcast_clojure_core_async/core.clj
@@ -43,66 +43,93 @@
     (go
       (time (take-part)))))
 
-(defn assembly-line []
-  (let [body-chan (chan)
-        wheel1-chan (chan)
-        wheel2-chan (chan)
-        body+wheel-chan (chan)
-        body+2-wheels-chan (chan)
-        box-chan (chan)]
-    (go
-      (while true
-        (let [body (loop []
-                     (let [part (take-part)]
-                       (if (body? part)
-                         part
-                         (recur))))]
-          (println "Got body")
-          (>! body-chan body))))
-    (go
-      (while true
-        (let [wheel1 (loop []
+(defn go-body [body-chan]
+  (go
+    (while true
+      (let [body (loop []
+                   (let [part (take-part)]
+                     (if (body? part)
+                       part
+                       (recur))))]
+        (println "Got body")
+        (>! body-chan body)))))
+
+(defn go-wheel1 [wheel1-chan]
+  (go
+    (while true
+      (let [wheel1 (loop []
                      (let [part (take-part)]
                        (if (wheel? part)
                          part
                          (recur))))]
-          (println "Got first wheel")
-          (>! wheel1-chan wheel1))))
-    (go
-      (while true
-        (let [wheel2 (loop []
-                       (let [part (take-part)]
-                         (if (wheel? part)
-                           part
-                           (recur))))]
-          (println "Got second wheel")
-          (>! wheel2-chan wheel2))))
+        (println "Got first wheel")
+        (>! wheel1-chan wheel1)))))
 
-    (go
-      (while true
-        (let [body (<! body-chan)
-              wheel1 (<! wheel1-chan)
-              bw (attach-wheel body wheel1)]
-          (println "Attached first wheel")
-          (>! body+wheel-chan bw))))
+(defn go-wheel2 [wheel2-chan]
+  (go
+    (while true
+      (let [wheel2 (loop []
+                     (let [part (take-part)]
+                       (if (wheel? part)
+                         part
+                         (recur))))]
+        (println "Got second wheel")
+        (>! wheel2-chan wheel2)))))
 
-    (go
-      (while true
-        (let [bw (<! body+wheel-chan)
-              wheel2 (<! wheel2-chan)
-              bww (attach-wheel bw wheel2)]
-          (println "Attached second wheel")
-          (>! body+2-wheels-chan bww))))
+(defn go-attach-wheel1 [body-chan wheel1-chan body+wheel-chan]
+  (go
+    (while true
+      (let [body (<! body-chan)
+            wheel1 (<! wheel1-chan)
+            bw (attach-wheel body wheel1)]
+        (println "Attached first wheel")
+        (>! body+wheel-chan bw)))))
 
-    (go
-      (while true
-        (let [(box-up (<! body+2-wheels-chan))]
-          (println "Boxed up car")
-          (>! box-chan box))))
+(defn go-attach-wheel2 [body+wheel-chan wheel2-chan body+2-wheels-chan]
+  (go
+    (while true
+      (let [bw (<! body+wheel-chan)
+            wheel2 (<! wheel2-chan)
+            bww (attach-wheel bw wheel2)]
+        (println "Attached second wheel")
+        (>! body+2-wheels-chan bww)))))
 
-    (go
-      (time
-       (dotimes [x 10]
-         (time
-          (put-in-truck (<! box-chan)))
-         (println "Done!!"))))))
+(defn go-box-up [body+2-wheels-chan box-chan]
+  (go
+    (loop []
+      (let [box (box-up (<! body+2-wheels-chan))]
+        (println "Boxed up car")
+        (when (>! box-chan box)
+          (recur))))
+    (async/close! body+2-wheels-chan)))
+
+(defn go-put-in-truck [box-chan]
+  (go
+    (time
+     (dotimes [x 10]
+       (time
+        (put-in-truck (<! box-chan)))
+       (println "Done!!")))
+    (async/close! box-chan)))
+
+(defn assembly-line []
+  (let [body-chan (chan 10)
+        wheel1-chan (chan 10)
+        wheel2-chan (chan 10)
+        body+wheel-chan (chan 10)
+        body+2-wheels-chan (chan 10)
+        box-chan (chan 10)]
+    (go-body body-chan)
+    (go-wheel1 wheel1-chan)
+    (go-wheel2 wheel2-chan)
+
+    (dotimes [x 2]
+      (go-attach-wheel1 body-chan wheel1-chan body+wheel-chan))
+
+    (dotimes [x 2]
+      (go-attach-wheel2 body+wheel-chan wheel2-chan body+2-wheels-chan))
+
+    (dotimes [x 2]
+      (go-box-up body+2-wheels-chan box-chan))
+
+    (go-put-in-truck box-chan)))
